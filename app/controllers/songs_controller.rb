b4ccde2c4c91b82b9aeecfc4e52ffb70518446e3
Admin <Tgafvert@gmail.com>
2017-08-17 13:45:16 -0400

    meh

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,9 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+    respond_to do |format|
+      format.json { render json: @songs }
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Song.delete_all
+
+Song.create(
+title: "a title",
+artist: "Artistto",
+album: "An album cover"
+)
